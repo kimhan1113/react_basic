3dd6cb2d5b1976dcd8fcf6e9d3900b5e5bb580c2
kimhan1113 <kimhan1113@naver.com>
2022-01-13 00:39:54 +0900

    state value commit

--- src/App.js
+++ src/App.js
@@ -3,44 +3,43 @@ import { Button } from "antd";
 import React from "react";
 // import "antd/dist/antd.css";
 
+const actions = {
+  init(initialValue) {
+    return { value: initialValue };
+  },
+  increment(prevState) {
+    return { value: prevState.value + 1 };
+  },
+  decrement(prevState) {
+    return { value: prevState.value - 1 };
+  },
+};
+
 class Counter1 extends React.Component {
+  state = actions.init(this.props.initialValue);
+
   state = {
     value: this.props.initialValue,
   };
 
-  onClick = () => {
-    const { value } = this.state;
-    this.setState({ value: value + 1 });
-  };
+  // onClick = () => {
+  //   const value = this.state.value + 1;
+  //   this.setState({ value });
+  // };
 
   render() {
     const { value } = this.state;
     return (
       <div>
         Counter1: {value}
-        <Button onClick={this.onClick}>+1</Button>
-      </div>
-    );
-  }
-}
-
-class FruitComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>좋아하는 과일</h1>
-        <ul>
-          {this.props.fruits.map((name, index) => (
-            <li key={index}>{name}</li>
-          ))}
-        </ul>
+        <Button onClick={() => this.setState(actions.increment)}>+1</Button>
+        <Button onClick={() => this.setState(actions.decrement)}>-1</Button>
       </div>
     );
   }
 }
 
 function App() {
-  const fruits = ["바나나", "사과", "딸기"];
   return (
     // <div>
     //   <button>Hello, React...</button>
@@ -51,7 +50,6 @@ function App() {
 
     <div>
       <Counter1 initialValue={10} />
-      <FruitComponent fruits={fruits} />
     </div>
   );
 }
